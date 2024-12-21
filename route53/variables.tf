1551b9c956533df869c8b16486c20f7595e1e034
JulioSM98 <juliosejas98@gmail.com>
2024-12-21 18:52:47 -0300

    feat(route53): Added new variable records in variables.tf for defining Route53 records with properties: zone_id, name, type, alias, records, ttl.

---
+++ route53/variables.tf
@@ -0,0 +1,19 @@
+variable "records" {
+  type = map(object({
+    zone_id = string
+    name    = string
+    type    = string
+
+    alias = object({
+      name    = string
+      zone_id = string
+      health  = bool
+    })
+
+    records = list(string)
+    ttl     = number
+
+    
+
+  }))
+}
