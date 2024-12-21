b0322e895590fda20591ef722964967f5513ad88
JulioSM98 <juliosejas98@gmail.com>
2024-12-21 19:05:01 -0300

    feat(ec2): Create EC2 instances with IAM roles using Terraform

---
+++ ec2/instances.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_instance_profile" "this" {
+  for_each = var.instance_profiles
+  name     = each.value.name
+  role     = each.value.role_name
+}
+
+resource "aws_instance" "this" {
+  for_each                    = var.instances
+  ami                         = each.value.ami_id
+  instance_type               = each.value.instance_type
+  subnet_id                   = each.value.subnet_id
+  associate_public_ip_address = each.value.associate_public_ip_address
+  iam_instance_profile        = aws_iam_instance_profile.this["${each.value.instance_profile_key}"].name
+  vpc_security_group_ids      = try([for key in each.value.security_groups_keys : aws_security_group.this["${key}"].id], null)
+  tags                        = merge({ Name = each.value.name }, each.value.tags)
+}
