906d4ec6e2c4860cd248ababbf0f11dd561937de
JulioSM98 <juliosejas98@gmail.com>
2024-12-21 19:06:33 -0300

    feat(ec2): Added variables for EC2 instances and instance profiles in vars_instances.tf

---
+++ ec2/vars_instances.tf
@@ -0,0 +1,21 @@
+variable "instances" {
+  type = map(object({
+    name                        = string
+    ami_id                      = string
+    instance_type               = string
+    subnet_id                   = string
+    associate_public_ip_address = bool
+    instance_profile_key        = string
+    security_groups_keys        = list(string)
+    tags                        = map(string)
+  }))
+  default = {}
+}
+
+variable "instance_profiles" {
+  type = map(object({
+    name      = string
+    role_name = string
+  }))
+  default = {}
+}
