a550f641d6dd2c1069112b4b131a195ef7d56edc
JulioSM98 <juliosejas98@gmail.com>
2024-12-21 19:19:10 -0300

    refactor(ec2): Update instances.tf with key_name attribute from key_pair_name

--- ec2/instances.tf
+++ ec2/instances.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "this" {
   subnet_id                   = each.value.subnet_id
   associate_public_ip_address = each.value.associate_public_ip_address
   iam_instance_profile        = aws_iam_instance_profile.this["${each.value.instance_profile_key}"].name
+  key_name                    = each.value.key_pair_name
   vpc_security_group_ids      = try([for key in each.value.security_groups_keys : aws_security_group.this["${key}"].id], null)
   tags                        = merge({ Name = each.value.name }, each.value.tags)
 }
